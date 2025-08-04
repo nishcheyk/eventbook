1246a54250233b8c4267491b620606227990ba22
nishcheyk <nishcheycapture2014@gmail.com>
2025-08-04 13:30:00 +0000

    changing author

--- app/express.d.ts
+++ app/express.d.ts
@@ -1,12 +1,10 @@
 import { ObjectId } from "mongoose";
-
 declare namespace Express {
   interface AuthenticatedUser {
     userId: string | ObjectId;
     email?: string;
     isAdmin?: boolean;
   }
-
   interface Request {
     user?: AuthenticatedUser;
   }
